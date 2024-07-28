3a4192c92a4dfe714556f3687b9a5899d8882c91
CJ Francisco <Drowzyking1126@gmail.com>
2024-07-28 09:39:11 +0800

    ready for deploy

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third partu
+    'crispy_forms',
+    'crispy_tailwind',
     # local apps
     'links',
 ]
@@ -124,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'tailwind'
+
+CRISPY_TEMPLATE_PACK = 'tailwind'
